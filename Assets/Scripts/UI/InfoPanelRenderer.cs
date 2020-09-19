c321cf7f0c8c2a20d49b2d47921fb77c5fd76004
Chloe Harris <chloecute0@gmail.com>
2020-09-19 14:37:43 +1000

    added ui structure for info panels

---
+++ Assets/Scripts/UI/InfoPanelRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanelRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private Text titleText = null;
+    [SerializeField]
+    private Text bodyText = null;
+
+    public string Title
+    {
+        get
+        {
+            return titleText.text;
+        }
+        set
+        {
+            titleText.text = value;
+        }
+    }
+
+    public string Body
+    {
+        get
+        {
+            return bodyText.text;
+        }
+        set
+        {
+            bodyText.text = value;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/UI/InfoPanelStack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanelStack : MonoBehaviour
+{
+    [SerializeField]
+    private InfoPanelRenderer panelPrefab;
+
+    private List<InfoPanelRenderer> panels;
+
+    public void AddPanel(string title, string body)
+    {
+        InfoPanelRenderer panel = Instantiate(panelPrefab, transform);
+        panel.Title = title;
+        panel.Body = body;
+        panels.Add(panel);
+    }
+
+    public void Clear()
+    {
+        while (panels.Count > 0)
+        {
+            InfoPanelRenderer panel = panels[0];
+            Destroy(panel);
+            panels.RemoveAt(0);
+        }
+    }
+
+
+
+}
