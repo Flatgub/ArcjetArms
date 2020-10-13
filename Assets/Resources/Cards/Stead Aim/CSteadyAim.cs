34f2fb9b92f45463aa705347db0c0ed1fba3e5ea
DZ1211 <62101699+DZ1211@users.noreply.github.com>
2020-10-13 17:19:37 +1100

    SteadyAim
    David: created steady aim. not sure if it is 100% complete since after saving it there were quite a lot of error of the object

---
+++ Assets/Resources/Cards/Stead Aim/CSteadyAim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//rename CardDataTemplate to the name of the card, like CStep or CSalvage
+public class CSteadyAim: CardData
+{
+    public override string GenerateStaticDescription()
+    {
+        return string.Format(descriptionTemplate);
+    }
+
+    public override string GenerateCurrentDescription()
+    {
+        return GenerateStaticDescription();
+    }
+
+    public override IEnumerator CardBehaviour(CardActionResult outcome)
+    {
+        GameplayContext.Player.ApplyStatusEffect(new SteadyAimEffect());
+        outcome.Complete();
+        yield break;
+    }
+
+}
---
+++ Assets/Resources/Cards/Stead Aim/SteadyAimStatusEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteadyAimStatusEffect : StatusEffect, IStatusAttackEventHandler,
+    IStatusCalculateDamageEventHandler, IStackableStatus
+{
+   
+    public int OnCalculateDamageAdditive(int damage)
+    {
+        return damage;
+    }
+    public float OnCalculateDamageMultiplicative(float damage)
+    {
+        if (GameplayContext.ActiveCard?.cardData.cardID == 14)
+        {
+            return damage * 2f;
+        }
+
+        return damage;
+    }
+    public void OnTurnEnd(Entity subject)
+    {
+        subject.RemoveStatusEffect(this);
+    }
+}
