79bc7c369f83640b0b75763c038e40da3b2f13e2
DZ1211 <62101699+DZ1211@users.noreply.github.com>
2020-10-14 18:34:44 +1100

    lineshot
    David: started lineshot, not completed. still trying to make headshot do 2x damage when below 50% hp

---
+++ Assets/Resources/Cards/CLineShot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//rename CardDataTemplate to the name of the card, like CStep or CSalvage
+public class CLineShot : CardData
+{
+    //put any numbers relevant to the card here
+    public int baseDamage;
+    public int damageDistance;
+    public int AfterDamage;
+    //generate the "basic" description, without doing any calculations.
+    public override string GenerateStaticDescription()
+    {
+        return string.Format(descriptionTemplate, baseDamage, damageDistance, AfterDamage); // <- put stat variables in here
+
+        {
+            if (GameplayContext.Player == null)
+                return GenerateStaticDescription();
+        }//todo add after damage of line shot
+    }
+    //what should the card do when its played, remember to do outcome.Complete or outcome.Cancel
+    public override IEnumerator CardBehaviour(CardActionResult outcome)
+    {
+        List<Hex> targetCandidates = new List<Hex>();
+        foreach (Hex dir in Hex.Directions)
+        {
+            List<Hex> line = GridHelper.CastLineInDirection(GameplayContext.Grid,
+                GameplayContext.Player.Position, dir, damageDistance, includeStart: false);
+            List<Entity> targets =
+                GridHelper.GetEntitiesInRange(GameplayContext.Grid, dir, range);
+            targetCandidates.AddRange(line);
+        }
+        targets.Remove(GameplayContext.Player);
+
+
+    }
+}
