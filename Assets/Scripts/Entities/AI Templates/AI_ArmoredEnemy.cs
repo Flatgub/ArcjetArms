4e30a402303139e43fc617eab8bf3837a55ab7c4
DZ1211 <62101699+DZ1211@users.noreply.github.com>
2020-10-21 14:46:42 +1100

    lancer and armored boi
    David: Added lancer and armored boi into templates

---
+++ Assets/Scripts/Entities/AI Templates/AI_ArmoredEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_ArmoredEnemy : IAiTemplate
+{
+	public int MaxHealth = 60;
+	public int moveSpeed = 3;
+	public int attackDamage = 5;
+
+	public void ApplyTo(Entity entity)
+	{
+		entity.entityName = "ArmoredEnemy";
+		entity.Health.SetMaxHealth(MaxHealth, updateHealth: true);
+		entity.AIController.AddAction(new MoveTowardsPlayer(moveSpeed), 1);
+		entity.AIController.AddAction(new ApproachMelee(moveSpeed - 1, attackDamage), 10);
+		entity.AIController.AddAction(new BasicMelee(attackDamage), 20);
+	}
+}
---
+++ Assets/Scripts/Entities/AI Templates/AI_Lancer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+	public class AI_Lancer : IAiTemplate
+	{
+		public int MaxHealth = 30;
+		public int moveSpeed = 3;
+		public int attackRange = 3;
+		public int RangeDamage = 3;
+		public int attackDamage = 7;
+
+		public void ApplyTo(Entity entity)
+		{
+			entity.entityName = "Lancer";
+			entity.Health.SetMaxHealth(MaxHealth, updateHealth: true);
+			entity.AIController.AddAction(new MoveWithinRangeOfPlayer(moveSpeed, attackRange), 7);
+			entity.AIController.AddAction(new BasicMelee(attackDamage), 10);
+			entity.AIController.AddAction(new BasicRanged(RangeDamage, attackRange), 5);
+		}
+	}
+
