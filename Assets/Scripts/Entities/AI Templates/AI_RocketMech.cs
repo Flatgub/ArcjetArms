c8c7db425e42085021dda0f913472e08ff626aad
Jayden <jayden.goncalves@students.mq.edu.au>
2020-10-26 14:01:49 +1100

    Added basic Rocket Mech
    Added Basic Rocket mech. Higher level replacement for Sniper.

---
+++ Assets/Scripts/Entities/AI Templates/AI_RocketMech.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_RocketMech : IAiTemplate
+{
+    public int MaxHealth = 20;
+    public int moveSpeed = 2;
+    public int attackDamage = 10;
+    public int attackRange = 6;
+
+    public void ApplyTo(Entity entity)
+    {
+        entity.entityName = "Rocket Deuce";
+        entity.Health.SetMaxHealth(MaxHealth, updateHealth: true);
+        entity.AIController.AddAction(new MoveWithinRangeOfPlayer(moveSpeed, attackRange), 1);
+        entity.AIController.AddAction(new BasicRanged(attackDamage, attackRange), 10);
+    }
+}
