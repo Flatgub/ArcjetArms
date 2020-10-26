4c88d642c2648f66b6b0bb5358f1b280ce88cb5a
DESKTOP-LKAU630\Tyler <tyler.de-souza@students.mq.edu.au>
2020-10-26 12:33:38 +1100

    added Mechanic enemy and healing ai

---
+++ Assets/Scripts/Entities/AI Templates/AI_Mechanic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Mechanic : IAiTemplate
+{
+    public int MaxHealth = 20;
+    public int moveSpeed = 2;
+    public int healAmount = 5;
+
+    public void ApplyTo(Entity entity)
+    {
+        entity.entityName = "Mechanic";
+        entity.Health.SetMaxHealth(MaxHealth, updateHealth: true);
+        entity.AIController.AddAction(new MoveTowardsEnemy(moveSpeed),1);
+        entity.AIController.AddAction(new ApplyHealing(healAmount), 1);
+    }
+}
