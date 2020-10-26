c367a9dba6c99f4bd76fe16bc5d52d311f4e5fb0
Jayden <jayden.goncalves@students.mq.edu.au>
2020-10-26 14:15:32 +1100

    Added Scavva
    Added a basic version of Scavva gonna need help on implementing a card scavange function

---
+++ Assets/Scripts/Entities/AI Templates/AI_Scavva.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Scavva : IAiTemplate
+{
+    public int MaxHealth = 30;
+    public int moveSpeed = 4;
+    public int attackDamage = 5;
+    public int attackRange = 1;
+    // Start is called before the first frame update
+
+    public void ApplyTo(Entity entity)
+    {
+        entity.entityName = "Scavva";
+        entity.Health.SetMaxHealth(MaxHealth, updateHealth: true);
+        entity.AIController.AddAction(new MoveWithinRangeOfPlayer(moveSpeed, attackRange), 1);
+        entity.AIController.AddAction(new BasicRanged(attackDamage, attackRange), 10);
+    }
+}
